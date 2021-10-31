23e68845c8f6b9abdc5f121fc92963a41c70a47e
HJ17J <57437315+HJ17J@users.noreply.github.com>
2021-11-01 05:38:09 +0900

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // 1초 기다리지 않고 바로 실행되게끔 함
+setInterval(getClock, 1000);
